08187b0b8ff1e203ac3254d5babbb1057ea87b06
Amey Latkar <ameylatkar7397@gmail.com>
2026-08-04 17:14:44 +0530

    feat: enhance dependency update action with input validation and improved workflow configuration

--- .github/actions/js-dependancy-update/index.js
+++ .github/actions/js-dependancy-update/index.js
@@ -1,6 +1,51 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const validateBranchName = ({ branchName }) => 
+    /^[a-zA-Z0-9_\/]+$/.test(branchName);
+
+const validateDirectoryName = ({ dirName }) =>
+    /^[a-zA-Z0-9_\/]+$/.test(dirName);
 
 async function run() {
+
+    const baseBranch = core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const workingDirectory = core.getInput('working-directory');
+    const ghToken = core.getInput('gh-token');
+    const debug = core.getBooleanInput('debug');
+
+    core.setSecret(ghToken);
+    if (validateBranchName({ branchName: baseBranch })) {
+        core.error(`Invalid base branch name: ${baseBranch}`);
+    }
+
+    if (validateBranchName({ branchName: targetBranch })) {
+        core.error(`Invalid target branch name: ${targetBranch}`);
+    }
+
+    if (validateDirectoryName({ dirName: workingDirectory })) {
+        core.error(`Invalid working directory name: ${workingDirectory}`);
+    }
+
+    core.info(`[js-dependency-update]:base branch is ${baseBranch}`);
+    core.info(`[js-dependency-update]:target branch is ${targetBranch}`);
+    core.info(`[js-dependency-update]:working directory is ${workingDirectory}`);
+
+    await exec.exec('npm update', [], {
+        cwd: workingDir
+    });
+
+    const gitStatus = await exec.getExecOutput('git status -s package*.json', [],{
+        cwd: workingDir
+    });
+
+    if (gitStatus.stdout.length > 0) {
+        core.info('[js-dependency-update]: There are updates available');
+    } else {    
+        core.info('[js-dependency-update]: No updates available');
+    }
+
     /*
         1.  Parse inpts:
             1.1 base-branch from which to check for updates
